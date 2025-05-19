5ee310227c9f06b672894aaca124b4c1fc18f430
Miyukiana <weber.selina@icloud.com>
2025-05-19 16:39:33 +0200

    platform (probably) added

--- script.js
+++ script.js
@@ -2,15 +2,18 @@ let player; // 360 | 455
 let jumpForce = 0;
 let ground; 
 let movement = 0;
+let platform;
 function setup(){
     createCanvas(720, 480);
  player = createVector(width / 2, height - 50);
+ platform = createVector(300, 150);
 ground = height - 50;
 }
 function draw(){
 background('SkyBlue');
 rectMode(CENTER);
 rect(player.x, player.y, 50, 50);
+rect(platform.x, platform.y, 100, 150);
 player.y += jumpForce;
 player.x += movement;  
 jumpForce += 0.5;
