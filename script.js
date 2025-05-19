f7fcd1ed5da489c5a552e3dba1773766dc4ca292
Miyukiana <weber.selina@icloud.com>
2025-05-19 16:48:46 +0200

    .

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function draw(){
 background('SkyBlue');
 rectMode(CENTER);
 rect(player.x, player.y, 50, 50);
+fill('red');
 rect(platform.x, platform.y, 100, 150);
 player.y += jumpForce;
 player.x += movement;  
@@ -21,7 +22,16 @@ jumpForce += 0.5;
 if (player.y >= ground){
     jumpForce = 0;
 
-}else {
+}else if (
+    player.x + 25 >= platform.x - 75 && 
+    player.x - 25 <= platform.x + 75 &&
+    player.y + 25 >= platform.y - 10 &&
+    player.y - 25 <= platform.y + 10
+){
+    jumpForce = 0;
+}
+}
+else {
     jumpForce += 0.5;
 }
 if (keyIsDown(RIGHT_ARROW)){
