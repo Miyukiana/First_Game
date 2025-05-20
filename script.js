2ffa5b3f71ef4a74c09079b3fc722bbc001d4765
Miyukiana <weber.selina@icloud.com>
2025-05-20 17:24:14 +0200

    .

--- script.js
+++ script.js
@@ -4,32 +4,34 @@ let ground;
 let movement = 0;
 let platform;
 function setup(){
-    createCanvas(720, 480);
- player = createVector(width / 2, height - 50);
+    createCanvas(600, 400);
+ player = createVector(250, 100);
  platform = createVector(300, 150);
 ground = height - 50;
 }
 function draw(){
 background('SkyBlue');
 rectMode(CENTER);
+fill('green');
 rect(player.x, player.y, 50, 50);
 fill('red');
 rect(platform.x, platform.y, 100, 150);
+
 player.y += jumpForce;
 player.x += movement;  
-jumpForce += 0.5;
+
+jumpForce += 0.5;   //?
 
 if (player.y >= ground){
     jumpForce = 0;
 
-}else if (
+}else if(
     player.x + 25 >= platform.x - 75 && 
     player.x - 25 <= platform.x + 75 &&
     player.y + 25 >= platform.y - 10 &&
     player.y - 25 <= platform.y + 10
 ){
     jumpForce = 0;
-}
 }
 else {
     jumpForce += 0.5;
@@ -44,4 +46,5 @@ if (keyIsDown(RIGHT_ARROW)){
 
 if (keyIsDown(32) && player.y >= ground){
     jumpForce = -12; 
+}
 }
